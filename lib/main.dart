96de8bdffb747c5966acf2c86680137a97725512
Oc-ean <michealolatunbosun603@gmail.com>
2023-09-27 18:55:55 +0100

    removed comments

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:movie_app/my_app.dart';
 
 void main() {
-  // runApp(
-  //   SplashScreen(
-  //     key: UniqueKey(),
-  //     onInitializationCompleted: () => runApp(
-  //       const ProviderScope(
-  //         child: MyApp(),
-  //       ),
-  //     ),
-  //   ),
-  // );
   runApp(
     ProviderScope(
       child: MyApp(),
